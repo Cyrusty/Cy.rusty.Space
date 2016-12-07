07103fed9ca1282a4f72113343c89da1b0d1e6a1
Aranad <aran@organicdesign.co.nz>
2016-12-07 07:19:21 -0200

    add git auto update

---
+++ site1/git-auto-update.php
@@ -0,0 +1,10 @@
+<?php
+if( array_key_exists( 'HTTP_X_HUB_SIGNATURE', $_SERVER ) ) {
+        $sig = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+        $body = file_get_contents( 'php://input' );
+        $hmac = hash_hmac( 'sha1', $body, file_get_contents( '/var/www/git-update-secret' ) );
+        if( $sig === "sha1=$hmac" ) {
+                $repo = json_decode( $body )->repository->name;
+                exec( "cd /var/www/cy.rusty.space && git pull --no-edit" );
+        }
+}
