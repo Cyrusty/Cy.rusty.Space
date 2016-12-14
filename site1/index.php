c4689487276a1a578b875a551faa9b4095b4dc42
Cyrusty <nzkr3w@gmail.com>
2016-12-14 17:53:42 +1300

    changes

--- site1/index.php
+++ site1/index.php
@@ -13,20 +13,6 @@
 </head>
 <body>
 <h1>Welcome to nginx on Debian Cyrustys site 1!!</h1>
-<?php
-$img_number = imagecreate(275,25);
-$backcolor = imagecolorallocate($img_number,102,102,153);
-$textcolor = imagecolorallocate($img_number,255,255,255);
-
-imagefill($img_number,0,0,$backcolor);
-$number = " Your IP is $_SERVER[REMOTE_ADDR]";
-
-Imagestring($img_number,10,5,5,$number,$textcolor);
-
-header("Content-type: image/jpeg");
-imagejpeg($img_number);
-?>
-
 <p style="color:red;"> yo yo yo this is another test edit using the HTML tutorials I'm working on
 check this link out <a href="http://www.organicdesign.co.nz"> OD </a>  </p> 
 <?php
