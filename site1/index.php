26197f5f59eb8ae739c88a5c92a77c544fa01e83
Cyrusty <nzkr3w@gmail.com>
2016-12-14 22:39:27 +1300

    added in visitors IP

--- site1/index.php
+++ site1/index.php
@@ -15,7 +15,7 @@
 <h1>Welcome to nginx on Debian Cyrustys site 1!!</h1>
 <?PHP
 
-echo Your IP address is $_SERVER['REMOTE_ADDR'];
+Your IP address is echo $_SERVER['REMOTE_ADDR'];
 
 ?>
 <p style="color:red;"> yo yo yo this is another test edit using the HTML tutorials I'm working on
