4f7378711fd76ac761a16e1db9d9d74b5b2d963c
Aranad <aran@organicdesign.co.nz>
2016-12-15 07:16:27 -0200

    revert to 1b2dd2e

--- site1/index.php
+++ site1/index.php
@@ -14,7 +14,7 @@
 <body>
 <h1>Welcome to nginx on Debian Cyrustys site 1!!</h1>
 <?php
-echo "Your IP address is " . $_SERVER ['REMOTE_ADDR'];
+echo "Your IP address is " . "<strong> $_SERVER ['REMOTE_ADDR'];</strong>
 ?>
 <p style="color:red;"> yo yo yo this is another test edit using the W3school tutorials I'm working on
 check this link out <a href="http://www.organicdesign.co.nz"> OD </a>  </p> 
