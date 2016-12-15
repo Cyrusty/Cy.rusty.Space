b6def10a6d53ee142fdeb7fbb7148d832036dfe0
Cyrusty <nzkr3w@gmail.com>
2016-12-15 17:21:28 +1300

    IP changes site

--- site1/index.php
+++ site1/index.php
@@ -29,15 +29,8 @@ echo "$x $y $x";
 ?>
 
 <!-- a comment for naran naran if he ever reads it -->
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a></p>
-
 <p>
-      Please use the <tt>reportbug</tt> tool to report bugs in the
-      nginx package with Debian. However, check <a
-      href="http://bugs.debian.org/cgi-bin/pkgreport.cgi?ordering=normal;archive=0;src=nginx;repeatmerged=0">existing
-      bug reports</a> before reporting a new bug.
-</p>
+     Goodmorning, good afternoon and goodnight </p>
 
 <p><em>Thank you for using debian and nginx AND REMOVING WINDOWS piece of shiat.</em></p>
 
