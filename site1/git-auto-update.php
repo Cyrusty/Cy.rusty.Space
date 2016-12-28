dfa95cb30edbeb5e86e4742fece263046e1f9ee3
Aranad <aran@organicdesign.co.nz>
2016-12-28 14:41:51 -0200

    rm pwd on update

--- site1/git-auto-update.php
+++ site1/git-auto-update.php
@@ -2,8 +2,9 @@
 if( array_key_exists( 'HTTP_X_HUB_SIGNATURE', $_SERVER ) ) {
         $sig = $_SERVER['HTTP_X_HUB_SIGNATURE'];
         $body = file_get_contents( 'php://input' );
-        $hmac = hash_hmac( 'sha1', $body, trim( file_get_contents( '/var/www/git-update-secret' ) ) );
-        if( 1||$sig === "sha1=$hmac" ) {
+        //$hmac = hash_hmac( 'sha1', $body, trim( file_get_contents( '/var/www/git-update-secret' ) ) );
+        //if( 1||$sig === "sha1=$hmac" ) {
+        if(1){
                 $repo = json_decode( $body )->repository->name;
                 exec( "cd /var/www/cy.rusty.space && sudo git pull --no-edit" );
         } else die( 'wrong pwd' );
