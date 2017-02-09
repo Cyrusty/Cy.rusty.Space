ca2ad12cf211d1d846ce06a1c2e4a98427e0a98c
Cyrusty <nzkr3w@gmail.com>
2017-02-10 07:10:44 +1300

    accordion sorted

--- site1/accordion.js
+++ site1/accordion.js
@@ -1,24 +1,15 @@
-window.onload = function() 
-	{
-		var acc = document.getElementsByClassName("accordion");
-}
-	{
-		var i;
-}
-	{
-		for (i = 0; i < acc.length; i++)
-}
-	{
-		acc[i].onclick = function() 
-}
-	{
-		this.classList.toggle("active");
-}
-	var panel = this.nextElementSibling;
-	if (panel.style.maxHeight)
-	{
-		panel.style.maxHeight = null;
-} else 
-	{
-	panel.style.maxHeight = panel.scrollHeight + "px";
+window.onload = function() {
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+	for (i = 0; i < acc.length; i++) {
+		acc[i].onclick = function() {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight){
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			}
+		}
+	}
 }
